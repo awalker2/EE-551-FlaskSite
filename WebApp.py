c76731a387874749afd2d1470e46a62d7ca6f453
Alex <awalker2@stevens.edu>
2018-03-20 22:39:45 -0400

    Got form and file upload working on Flask

--- WebApp.py
+++ WebApp.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('home.html')
+
+@app.route('/generate', methods = ['POST'])
+def generate():
+    print request.files
+    transcript = request.files.get('formTranscript')
+    print transcript.read()
+    data = request.form
+    for key in data:
+        print key + ": " + data[key]
+    return "success"
 
 if __name__ == "__main__":
     app.run()
